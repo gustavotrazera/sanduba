bd877b379c0260a26a55b2605b9ba2dc518c4799
Gustavo Teller Razera <gustavoteller@gmail.com>
2020-01-11 12:06:27 -0300

    Commit inicial do projeto sanduba

---
+++ src/main/java/com/br/sanduba/sanduba/SandubaApplication.java
@@ -0,0 +1,13 @@
+package com.br.sanduba.sanduba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandubaApplication.class, args);
+	}
+
+}
